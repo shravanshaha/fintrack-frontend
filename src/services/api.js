8ecbed42c2028023acdd888141fac09351e73bfe
shravan <shravanshaha18@gmail.com>
2026-06-05 23:09:22 +0530

    configure production backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/fintrack",
+  baseURL: "https://fintrack-backend-c16m.onrender.com",
   withCredentials: true
 });
 
